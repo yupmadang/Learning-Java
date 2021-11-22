12e56c5ded2d3f27507999049fc8dc6fe71cb24c
Yongjin Jeon <yjin1185@gmail.com>
2021-11-22 11:36:55 +0900

    java chapter6 part1 2021.11.22

---
+++ Practice_Java/src/StudentTest.java
@@ -0,0 +1,8 @@
+public class StudentTest {
+    public static void main (String[] args){
+        chapter6 std1 = new chapter6();
+        std1.studentName = "ȫ�浿";
+        std1.studentNum = 123456789;
+        std1.showINfo();    
+    }  
+}
---
+++ Practice_Java/src/chapter6.java
@@ -0,0 +1,11 @@
+public class chapter6 {
+
+    int studentID;
+    String studentName;
+    int studentNum;
+    String address;
+
+    public void showINfo(){
+        System.out.println(studentName+ "," +studentNum);
+    }
+}
