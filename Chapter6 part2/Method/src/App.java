6e33f2089311b63236e8de2258ebf22437c2360a
Yongjin Jeon <yjin1185@gmail.com>
2021-11-22 15:28:14 +0900

    chapter5 5-1연습문제

--- Chapter6 part2/Method/src/App.java
+++ Chapter6 part2/Method/src/App.java
@@ -1,13 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int num1 = 15;
-        int num2 = 20;
 
-        Calculater calc = new Calculater();
-        System.out.println(calc.Case1(num1, num2));
-        System.out.println(calc.Case2(num1, num2));
-        System.out.println(calc.Case3(num1, num2));
-        System.out.println(calc.Case4(num1, num2));
-        System.out.println(calc.Case5(num1, num2));
+        Calculater calc = new Calculater(15,20);
+        calc.Function(calc.num1, calc.num2, 1);
+
+        /*계산기 생성문제 5-1번 문제*/
+        /*Function함수 끝에 정수형 변수를 이용한 사칙연산 종류 선택가능*/
     }
 }
--- Chapter6 part2/Method/src/Calculater.java
+++ Chapter6 part2/Method/src/Calculater.java
@@ -1,22 +1,48 @@
 public class Calculater {
-    int Case1 (int num1, int num2){
-        return num1 + num2;
+    int num1;
+    int num2;
+
+    public Calculater (int num1 , int num2){  /*생성자 형성*/
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    
+    public int getNum1(){ /*변수를 반환받기 위한 get메서드 선언*/
+        return num1;
     }
 
-    int Case2 (int num1, int num2){
-        return num1 - num2;
+    public void setNum1(int num1){ /*생성자에 변수를 입력하기 위한 set메서드 선언*/
+        this.num1 = num1;
     }
 
-    int Case3 (int num1, int num2){
-        return num1 * num2;
+    public int getNum2(){
+        return num2;
     }
 
-     float Case4 (int num1, int num2){
-        return num1 / num2;
+    public void setNum2(int num2){
+        this.num2 = num2;
     }
 
-    int Case5 (int num1, int num2){
-        return num1 % num2;
-    }
+    void Function (int num1, int num2, int type){
+        float result;
+         switch (type) { /*스위치문을 이용한 사칙연상 함수 설정*/
+            case 1 : result = num1 + num2;
+                     System.out.println(result);
+                     break;
 
+            case 2 : result = num1 - num2;
+                     System.out.println(result);
+                     break;
+
+            case 3 : result = num1 / num2;
+                     System.out.println(result);
+                     break;
+
+            case 4 : result = num1 % num2;
+                     System.out.println(result);
+                     break;
+                     
+            default : System.out.println("적용되는 모드는 없습니다.");
+         }
+    }
 }
