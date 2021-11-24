6b69ce5e6ff7467204cc2c4302bd54769343775d
Yongjin Jeon <yjin1185@gmail.com>
2021-11-24 17:53:13 +0900

    Chapter8 part1 ex) 2021. 11. 24 배열복사1

--- Chapter8 part1/BookArray/src/Book.java
+++ Chapter8 part1/BookArray/src/Book.java
@@ -2,10 +2,15 @@ public class Book {
     private String bookName; /*외부에서 이 맴버변수를 직접 참조하여 사용할 수 없음.*/
     private String author;
 
+    /*자바에서 프로그래밍을 할 때는 후에 나올 캡슐화와 정보은닉을 원칙으로 한다고 합니다.
+    이는 자바가 지향하는 무결성을 보장하는 측면이 있음 .......무결성은 두루뭉실하게 알고 있어 추가 학습필요*/
+
     public Book(String bookName, String author){
         this.bookName = bookName;
         this.author = author;
     }
+
+    public Book(){};
 
     public String getBookName(){
         return bookName;
