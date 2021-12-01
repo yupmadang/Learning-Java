33de881881f60793270abe9bfc06b4b79fb025f9
Yongjin Jeon <yjin1185@gmail.com>
2021-12-01 13:27:52 +0900

    2021. 12. 01 hashCode 예제

--- Chapter12/equal/src/Student.java
+++ Chapter12/equal/src/Student.java
@@ -11,6 +11,10 @@ public class Student {
         return studentName +","+ studentID;
     }
 
+    public int hashCode(){ /*해시코드 값으로 학번을 반환하는 메서드 재정의*/
+        return studentID;
+    }
+
     public boolean equals(Object obj){ /*equals()메서드를 재정의하여 인스턴스의 주소가 아닌 studentID가 같으면 true를 반환하도록 재정의*/
         if(obj instanceof Student){
             Student std = (Student)obj;
@@ -56,9 +60,20 @@ public class Student {
         else{
             System.out.println("두 인스턴스는 다릅니다.");
         }
+
+        System.out.println(std1.hashCode()); /*재정의한 메서드로 반환*/
+        System.out.println(std3.hashCode());
+
+        System.out.println(System.identityHashCode(std1)); /*실제 인스턴스 주소를 반환*/
+        System.out.println(System.identityHashCode(std3));
+    
     }
 }
 
 /*기본적으로 equal()메서드는 인스턴스의 주소를 비교하여 같은 인스턴스인지 확인하나
 인스턴스의 주소가다른 경우에도 동일한 인스턴스인 경우가 있다. 이를 논리적 동일성이라하고
 이 경우 equal(메서드를 재정의하여 물리적 동일성만이 아닌 논리적 동일성도 확보해야 한다.*/
+
+/*hashCode()메서드는 기본적으로 객체의 저장위치를 반환하녀 정보의 검색 등에 활용한다.
+equals로 재정의하여 동일한 인스턴스라고하면 해시코드의 값도 같은 값을 출력하도록 재정의가 
+필요하다*/
